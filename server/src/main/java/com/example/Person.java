83222ff546d200457c061102325a9bdb4063b100
slowjams <lxdthriller@gmail.com>
2024-03-23 15:20:17 +1100

    more

--- server/src/main/java/com/example/Person.java
+++ server/src/main/java/com/example/Person.java
@@ -12,6 +12,10 @@ public class Person {
 
   }
 
+  public String person2(String someone) {
+    return String.format("Hello, %s!", someone);
+  }
+
   //TODO: Add javadoc comment
   public String person(String someone) {
     return String.format("Hello, %s!", someone);
